f87d3e273521b21269125da85e4c5234401f0d8b
Oscar Bolanos <oscarbol98@gmail.com>
2026-04-19 22:16:17 -0500

    add variables

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL  = 'https://mfvbtlormnveloqnzeqf.supabase.co'
-const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1mdmJ0bG9ybW52ZWxvcW56ZXFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU1MDc2ODIsImV4cCI6MjA5MTA4MzY4Mn0.54iLaKQr9xZgo_ttm_dcWglEaJ7KukzxAsXLswBj_Uc'
+const SUPABASE_URL  = import.meta.env.VITE_SUPABASE_URL
+const SUPABASE_ANON = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON)
